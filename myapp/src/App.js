d23d4d6a1d56a641e21da168da5d9bded6b4d3d6
milla56 <lubomila14@gmail.com>
2023-06-12 00:11:34 +0100

    navbar made

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+
+import Home from '../src/pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+      
      
     </div>
   );
--- myapp/src/pages/Home.js
+++ myapp/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from '../components/Navbar'
 
 
-function Home() {
+const Home = () => {
   return (
     <div className="Home">
      
