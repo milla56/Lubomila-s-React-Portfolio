e93d93ae2900a228a9d5c0e2d9d837ce6702f4d0
milla56 <lubomila14@gmail.com>
2023-06-12 10:53:07 +0100

    contact page made

--- myapp/src/pages/Contact.js
+++ myapp/src/pages/Contact.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Container,FormControl,Label } from "react-bootstrap";
 
 
-function Contact() {
+const Contact = () => {
   return (
     <div className="Contact">
+<Container> 
+<h2 className="mb-3">Send A Message</h2>
+    <form>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="name">
+          Name
+        </label>
+        <input className="form-control" type="text" id="name" required />
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+        <input className="form-control" type="email" id="email" required />
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="message">
+          Message
+        </label>
+        <textarea className="form-control" id="message" required />
+      </div>
 
+      <button className="btn btn-primary" type="submit">
+       
+      </button>
+    </form>
+</Container>
     </div>
   );
 }
