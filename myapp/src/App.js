10b000adcff7211835c7dc750292f3b22e85a552
milla56 <lubomila14@gmail.com>
2023-06-13 19:38:52 +0100

    react router changed app.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
 import './App.css';
 
 import Home from './pages/Home'
@@ -9,11 +9,13 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Router>
+        <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       </Routes>
+      </Router>
       
      
     </div>
