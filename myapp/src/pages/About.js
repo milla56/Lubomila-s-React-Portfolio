bf02470ceca43a6aebf843facbc049e84c0fa38c
milla56 <lubomila14@gmail.com>
2023-06-15 21:26:05 +0100

    about me.page started to get styled

--- myapp/src/pages/About.js
+++ myapp/src/pages/About.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import "../styles/about.css"
+
+import { Container} from "react-bootstrap";
 
 // Images
 import myphoto from '../images/myphoto.jpg'
@@ -15,21 +18,32 @@ const About = () => {
   return (
     <div className='aboutme'>
     <h1>About Me</h1>
-    <img 
-    className = "img-fluid rounded-circle"
-    src={myphoto} 
-    alt="Avatar" />
-    <p>
-    Graphic Designer and Junior web Developer experienced in managing multiple websites 
-    and web application design projects. Proficient at developing creative and functional designs, web page layout, 
-    creating user interfaces, writing and testing codes, and infrastructure to maintain a cohesive website consistent with clients needs.
-    </p>
-    <a href="https://drive.google.com/file/d/16wPZr0HDKNc37wzPJw1Gby4o_EkPzulF/view" 
-    className="btn1 btn-lg btn-outline-light">
-            <i className="fa fa-download" aria-hidden="true" id="download"></i>
-             Download my Resume!</a>
+    <Container className='info'> 
+    <div className="row align-items-center" id="pannel">
+        <div className="col-lg-5 order-lg-2">
+          <div className="p-5">
+            <img className="img-fluid rounded-circle" src={myphoto} alt="self portrait"/>
+          </div>
+        </div>
+        <div class="col-lg-7 order-lg-1">
+          <div class="p-5">
+            <h3 class="name">Get To Know Me</h3>
+            <p>Front End Web Developer experienced in managing multiple websites 
+                and web application design projects. 
+                Proficient at developing creative and functional designs, web page layout, 
+                creating user interfaces, writing and testing codes, 
+                and infrastructure 
+                to maintain a cohesive website consistent with clients needs.</p>
+            <a href="https://drive.google.com/file/d/1ekJTwcekC7yv7IO-f22gjGo4TpjPtIDB/view?usp=share_link" class="btn1 btn-lg btn-outline-light">
+              <i class="fa fa-download" aria-hidden="true" id="download"></i>
+               Download my Resume!</a>
+  
 
-  <h2> My Skills</h2>
+          </div>
+        </div>
+      </div>
+      </Container>
+  {/* <h2> My Skills</h2>
   <img 
     className = "html"
     src={html} 
@@ -63,10 +77,14 @@ const About = () => {
    <img 
     className = "adobe"
     src={adobe} 
-    alt="adobe,icon" />
+    alt="adobe,icon" /> */}
   </div>
  
   );
 }
 
 export default About;
+
+
+
+
